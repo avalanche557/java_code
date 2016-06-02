e29b77f43e873a6739d19ae21464815a9be56042
abhinav <abhinavkarn55@gmail.com>
2016-06-02 12:59:08 -0700

    7rd

---
+++ insert_node.java
@@ -0,0 +1,37 @@
+
+public class insert_node {
+	node head;
+	
+	class node{
+		int data;
+		node next;
+		
+		node(int d) {
+			data = d;
+			next = null;
+		}	
+	}
+	public void push(int new_data) {
+		node new_node = new node(new_data);
+		new_node.next = head;
+		head = new_node;
+	}
+	
+	public void printlist() {
+		node n = head;
+		while(n != null) {
+			System.out.println(n.data+" ");
+			n= n.next;
+		}
+	}
+	public static void main(String[] args) {
+		insert_node list = new insert_node();
+		
+		list.push(3);
+		list.push(4);
+		list.push(5);
+		
+		System.out.println("this linkedlist is ");
+		list.printlist();
+	}
+}
