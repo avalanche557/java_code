c3088cd9f423dfa1b54c3f5f3a9175c949993c75
abhinav <abhinavkarn55@gmail.com>
2016-06-08 00:20:25 -0700

    20th

--- MergSort.java
+++ MergSort.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class MergSort {
-	node head;
+	static node head;
 	
 	static class node{
 		int data;
@@ -17,6 +17,7 @@ public class MergSort {
 		new_node.next = head;
 		head =new_node;
 	}
+
 	public void printlist() {
 		node n = head;
 		while(n != null){
@@ -24,6 +25,46 @@ public class MergSort {
 			n= n.next;
 		}
 	}
+	public static node getmid(node head){
+		node fast = head;
+		node slow = head;
+		while(slow.next != null && fast.next.next != null){
+			fast= fast.next.next;
+			slow = slow.next;
+			
+		}
+		return slow;
+
+	}
+	public node merg(node head) {
+		if(head == null || head.next == null){
+			return head;
+		}
+		node midnode = getmid(head);
+		node secondlist = midnode.next;
+		midnode.next = null;
+		return mergsort(merg(head), merg(secondlist));
+	}
+	
+	public static node mergsort(node h1, node h2) {
+		node result;
+		if(h1 == null){
+			return h2;
+		}
+		if(h2 == null){
+			return h1;
+		}
+		if(h1.data <= h2.data){
+			result = h1;
+			result.next = mergsort(h1.next, h2);
+		}
+		else {
+			result = h2;
+			result.next = mergsort(h1, h2.next);
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		MergSort list = new MergSort();
 		
@@ -33,7 +74,7 @@ public class MergSort {
 		System.out.println("unsorted list is");
 		list.printlist();
 		
-		
+		list.merg(head);
 		System.out.println("sorted list is");
 		list.printlist();
 	}
