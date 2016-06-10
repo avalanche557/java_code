3303ec9171b5a2e2fff4e6407fc50de4d9cfb896
abhinav <abhinavkarn55@gmail.com>
2016-06-10 00:03:42 -0700

    223h

--- CircularList.java
+++ CircularList.java
@@ -1,34 +1,43 @@
 import java.util.Random;
 
 public class CircularList {
-node head;
- static class node {
-	 int data;
-	 node next;
-	 node (int d){
-		 data = d;
-	 }
- }
- public void push(int new_data) {
-	node new_node = new node(new_data);
-
-	new_node.next = head;
-	head = new_node;
-	
-}
- 
- public void printlist() {
-	
-}
- public static void main(String args[]) {
-	CircularList list = new CircularList();
-	
-	for(int i=0; i < 10; i++){
-		list.push(new Random().nextInt(6));
+	node head;
+	static class node {
+		int data;
+		node next;
+		
 	}
-	System.out.println("the circlular linked list is ");
-	list.printlist();
-	
-}
- 
+	public void push(int new_data) {
+		node new_node = new node();
+		new_node.data = new_data;
+		new_node.next = head;
+		node temp = head;
+		if(head == null) {
+			new_node.next = new_node;
+		}
+		else {
+		while(temp.next != head){
+			temp = temp.next;
+		}
+		temp.next = new_node;
+		}
+		head = new_node;
+		
+	}
+	public void printlist() {
+		node n = head;
+		while(n.next != head){
+			System.out.println(n.data +" ");
+			n = n.next;
+		}
+	}
+	public static void  main(String args[]) {
+		CircularList list = new CircularList();
+		
+		for(int i =0; i < 10; i++){
+			list.push(new Random().nextInt(10));
+		}
+		System.out.println("the circular linked list is");
+		list.printlist();
+	}
 }
