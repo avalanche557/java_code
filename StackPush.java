7b88e5d5dcd9b92083cf9633789e18262af18253
abhinav <abhinavkarn55@gmail.com>
2016-06-11 22:37:34 -0700

    34th

--- StackPush.java
+++ StackPush.java
@@ -1,4 +1,39 @@
 
 public class StackPush {
+	static int size;
+	static int[] array;
+	int top;
+	public StackPush(int s) {
+		// TODO Auto-generated constructor stub
+		size = s;
+		array = new int[size];
+		top = -1;
+	}
+	public void push(int new_data) {
+		array[++top] = new_data;
+	}
+	public int pop() {
+		return array[top--];
+	}
+	public static void main(String args[]) {
+		StackPush stack = new StackPush(6);
+		int i = 0;
+		while(i != size){
+			stack.push(i);
+			System.out.println(array[i]);
+			i++;
+		}
+		System.out.println("pop function is used");
+		while(size != 0){
+			int value = stack.pop();
+			System.out.printf("%d\n", value);
+			size--;
+		}
+		
+		
+		
+		
+	}
+	
 
 }
